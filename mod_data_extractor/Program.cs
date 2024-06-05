61cbc73ad53c1a9524e39d415a9ec82bc9be5710
jordan.dalby <jordan.dalby1@outlook.com>
2024-06-05 18:35:13 +0100

    Semantics

--- mod_data_extractor/Program.cs
+++ mod_data_extractor/Program.cs
@@ -10,27 +10,25 @@ namespace UtocDumper
 
         static void Main(string[] args)
         {
-            AbstractVfsFileProvider Provider;
-
             // Where the server is stored on disk, needs to be the main directory so that it can find the globals.utoc file
-            const string path = @"C:\Users\jorda\Desktop\TestServer\testserver";
+            const string path = @"C:\Users\jorda\Desktop\CSMDE";
             // The mod to look for, not strictly necessary but good for reducing required operations
             const string modOfInterest = "ResourceGatherers";
 
             // Create the default file provider from the base directory, basically does everything for you
-            Provider = new DefaultFileProvider(directory: path, searchOption: SearchOption.AllDirectories, isCaseInsensitive: true, versions: new VersionContainer(EGame.GAME_UE5_0));
+            AbstractVfsFileProvider provider = new DefaultFileProvider(directory: path, searchOption: SearchOption.AllDirectories, isCaseInsensitive: true, versions: new VersionContainer(EGame.GAME_UE5_0));
 
             // Initialize and mount the file provider
-            Provider.Initialize();
-            Provider.Mount();
+            provider.Initialize();
+            provider.Mount();
 
             // Not necessary but will keep here in case it's useful in some way
             // IoStoreReader reader = new IoStoreReader(Directory.GetFiles(path, "global.utoc", SearchOption.AllDirectories)[0], CUE4Parse.UE4.IO.Objects.EIoStoreTocReadOptions.ReadDirectoryIndex, new VersionContainer(EGame.GAME_UE5_0));
             // IoGlobalData ioGlobalData = new IoGlobalData(reader);
 
-            Console.WriteLine($"Found {Provider.Files.Count} files");
+            Console.WriteLine($"Found {provider.Files.Count} files");
 
-            foreach (var entry in Provider.Files.Values)
+            foreach (var entry in provider.Files.Values)
             {
                 // perhaps not foolproof, relies on the name of the mod being in the path
                 // this condition isn't strictly necessary, but it certainly cuts down the amount of data that needs to be combed through
@@ -45,7 +43,7 @@ namespace UtocDumper
                     try
                     {
                         // Load all of the objects from the uasset file
-                        var exports = Provider.LoadAllObjects(entry.Path);
+                        var exports = provider.LoadAllObjects(entry.Path);
                         // Serialize to string and write
                         Console.WriteLine($"{entry.Name} \n{JsonConvert.SerializeObject(exports, Formatting.Indented)}");
                     }
