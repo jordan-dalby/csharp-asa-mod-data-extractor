95d1f7d398339d6ced1495277d84bdc6e2e6b35a
jordan.dalby <jordan.dalby1@outlook.com>
2024-06-09 22:32:13 +0100

    Added recursive search for parents, ensuring objects have all values that they inherit from parents

--- mod_data_extractor/Program.cs
+++ mod_data_extractor/Program.cs
@@ -2,8 +2,11 @@
 using CUE4Parse.UE4.Versions;
 using CUE4Parse.FileProvider.Vfs;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
 using CommandLine;
+using CUE4Parse.UE4.Assets.Exports;
+using CUE4Parse.UE4.Assets;
 
 namespace UtocDumper
 {
@@ -103,6 +106,17 @@ namespace UtocDumper
                 {
                     // Load all of the objects from the uasset file
                     var exports = provider.LoadAllObjects(entry.Path);
+
+                    // UObjects themselves actually on contain the changes from the parent, so to get all values we need
+                    // to get values from the parents, as deep as it goes, the following handles that
+                    List<JObject> data = new List<JObject>();
+                    foreach (UObject export in exports)
+                    {
+                        List<UObject> makeup = GetClassMakeup(export);
+                        JObject fullClassData = CombineClassData(makeup);
+                        data.Add(fullClassData);
+                    }
+
                     // Concat output file path
                     string filePath = Path.Combine(outputPath, entry.PathWithoutExtension + ".json");
                     // Create missing directories if not exists
@@ -114,13 +128,61 @@ namespace UtocDumper
                     }
                     Directory.CreateDirectory(directoryName);
                     // Serialize to string and write
-                    File.WriteAllText(filePath, JsonConvert.SerializeObject(exports, Formatting.Indented));
+                    File.WriteAllText(filePath, JsonConvert.SerializeObject(data, Formatting.Indented));
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Exception caused by {entry.Name} \nException: {ex.Message}");
                 }
             }
+        }
+
+        public static List<UObject> GetClassMakeup(UObject export)
+        {
+            List<UObject> result = new List<UObject>() { export };
+            // If the object has no parent, just return the result
+            if (export.Template == null)
+                return result;
+            // Get the parent object
+            ResolvedObject currentObject = export.Template;
+            while (currentObject != null)
+            {
+                // Get the actual parent object
+                UObject parent;
+                if (currentObject.TryLoad(out parent))
+                {
+                    // Add it to the result
+                    result.Add(parent);
+                    // If this parent has no parent, it's the highest, so break
+                    if (parent.Template == null)
+                        break;
+                    // Set the current object
+                    currentObject = parent.Template;
+                }
+            }
+            // Need to go in reverse order, starting with the highest parent, finishing with the object we care about
+            result.Reverse();
+            return result;
+        }
+
+        private static JObject CombineClassData(List<UObject> classData)
+        {
+            // If there's no class data, just return an empty object
+            if (classData.Count == 0)
+                return new JObject();
+
+            // Create a JObject from the first object in the array, then loop everything else
+            JObject result = JObject.FromObject(classData[0]);
+            for (int i = 1; i < classData.Count; i++)
+            {
+                // Get the object & convert to JObject
+                UObject obj = classData[i];
+                JObject jsonObj = JObject.FromObject(obj);
+
+                // Merge into the result
+                result.Merge(jsonObj);
+            }
+            return result;
         }
     }
 }
